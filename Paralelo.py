b4cd7ebe54eaab14040d85f53a2bbfa3b3137906
HDuda99 <111542543+duda-honorato@users.noreply.github.com>
2025-11-13 23:44:14 -0300

    Implement pi_paralelo function for pi estimation
    Add parallel computation function for estimating pi.

--- Paralelo.py
+++ Paralelo.py
@@ -1,8 +1,14 @@
 #cdg em andamento, nada disso foi testado
+#Ref https://materials.jeremybejarano.com/MPIwithPython/introMPI.html -https://fbpic.github.io/_modules/fbpic/utils/random_seed.html
 
 import MPI #verificar se eh assim msm
 import random
 import time
 import math
 
-def 
+def pi_paralelo(n_pontos):
+  pontos_dentro = 0
+  pontos_fora = 0
+  #Incluir Seed para meio que separar os processos, sem isso tera processos duplicados
+  random.seed(MPI.COMM_WORLD.Get_rank() + int(time.time()))
+  for_in rab
